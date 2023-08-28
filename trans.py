ad60271d59976cc89bc4bd124606b15eefd48565
naoki matsuo <n-matsuo@g-unity.jp>
2023-08-28 14:52:12 +0900

    commit

--- trans.py
+++
@@ -1,8 +0,0 @@
-import whisper
-import sys
-
-args = sys.argv
-# 利用できる学習済みmodelサイズは tiny, base, small, medium, largeの順に5種類
-model = whisper.load_model(args[1])
-result = model.transcribe(args[2], verbose=True, language='ja')
-print(result["text"])
